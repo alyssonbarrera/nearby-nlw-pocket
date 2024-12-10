6e7b5ff602dad5361227d6369474a8785db66b4c
Alysson Barrera <alyssonbarrera@outlook.com>
2024-12-09 21:23:08 -0300

    🎉 initial commit

---
+++ NLW/Sources/Features/Coordinator/NearbyFlowController.swift
@@ -0,0 +1,24 @@
+//
+//  NearbyFlowController.swift
+//  NLW
+//
+//  Created by Alysson Barrera on 09/12/24.
+//
+
+
+import Foundation
+import UIKit
+
+class NearbyFlowController {
+    private var navigationController: UINavigationController?
+    
+    public init() {}
+    
+    func start() -> UINavigationController? {
+        let contentView = SplashView()
+        let startViewController = SplashViewController(contentView: contentView)
+        self.navigationController = UINavigationController(rootViewController: startViewController)
+
+        return navigationController
+    }
+}
